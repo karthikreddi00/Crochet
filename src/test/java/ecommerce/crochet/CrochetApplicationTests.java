3107a9f564627cd129c0b94e2daea1ce629f3657
Karthik Reddy <kartheekreddy2605@gmai.com>
2026-09-14 14:31:53 +0530

    initial

---
+++ src/test/java/ecommerce/crochet/CrochetApplicationTests.java
@@ -0,0 +1,13 @@
+package ecommerce.crochet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrochetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
